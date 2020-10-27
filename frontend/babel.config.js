a61e2d26ccc9635ad90e3f2bcdc073224661068a
Jeongeun-Choi <cje3080@gmail.com>
2020-10-27 15:59:58 +0900

    feat[#6]: FE 추가 설정 - babel.config.js: 오류 해결 - webpack.config.js: devServer 옵션 추가 - .prettierrc: 코드 통일

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -1,15 +1,3 @@
 module.exports = {
-  presets: [
-    '@babel/preset-react',
-    [
-      '@babel/preset-env',
-      {
-        targets: '> 2%, not dead',
-        corejs: 3,
-        useBuiltIns: 'usage',
-        modules: false,
-        shippedProposals: true,
-      },
-    ],
-  ],
+  presets: ['@babel/preset-react', '@babel/preset-env']
 };
