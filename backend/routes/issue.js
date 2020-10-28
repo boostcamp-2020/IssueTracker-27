49882ce3075801a2029410926d5bea242c942a09
김종은 <jongeunkim@gimjong-eun-ui-MacBookPro.local>
2020-10-28 17:56:50 +0900

    fix #9: issue의 openAt 필드에 오늘 날짜를 default 값으로 설정
    issue를 생성할 때 openAt 값이 자동으로 생성되도록 sequelize에서 설정.
    joinUser와 issue의 foreignkey 값이 null이면 안되도록 설정값을 통일.

---
+++ backend/routes/issue.js
@@ -0,0 +1,91 @@
+const express = require('express');
+const router = express.Router();
+const { db } = require('../models/index');
+
+router.post('/', async (req, res) => {
+  const {
+    title,
+    content,
+    milestoneId,
+    issueTrackerId,
+    labelIds = [],
+    joinUserIds = [],
+  } = req.body;
+  try {
+    const newIssue = await db.issue.create({
+      title,
+      content,
+      milestoneId,
+      issueTrackerId,
+    });
+
+    await Promise.all([
+      db.issueLabel.bulkCreate(
+        labelIds.map((labelId) => ({ labelId, issueId: newIssue.id }))
+      ),
+      db.assignee.bulkCreate(
+        joinUserIds.map((joinUserId) => ({ joinUserId, issueId: newIssue.id }))
+      ),
+    ]);
+
+    return res.status(200).json({ message: 'issue가 만들어졌습니다.' });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({ message: 'error' });
+  }
+});
+
+router.patch('/:issueId', async (req, res) => {
+  const { issueId } = req.params;
+  const {
+    title,
+    content,
+    deleteIssueLabelIds,
+    addLabelIds,
+    milestoneId,
+    deleteAssigneeIds,
+    addJoinUserIds,
+  } = req.body;
+  try {
+    await Promise.all([
+      db.assignee.destroy({
+        where: {
+          id: deleteAssigneeIds,
+        },
+      }),
+      db.assignee.bulkCreate(
+        addJoinUserIds.map((joinUserId) => ({
+          joinUserId,
+          issueId,
+        }))
+      ),
+      db.issueLabel.destroy({
+        where: {
+          id: deleteIssueLabelIds,
+        },
+      }),
+      db.issueLabel.bulkCreate(
+        addLabelIds.map((labelId) => ({ labelId, issueId }))
+      ),
+      db.issue.update(
+        {
+          title,
+          content,
+          milestoneId,
+        },
+        {
+          where: {
+            id: issueId,
+          },
+        }
+      ),
+    ]);
+
+    res.status(200).json({ message: 'issue가 업데이트 되었습니다.' });
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({ message: 'error' });
+  }
+});
+
+module.exports = router;
