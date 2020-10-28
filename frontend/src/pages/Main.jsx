12490bdfbbfd2f0cbd3a92acd8ecee06a2cbdf42
geonwoomun <38036598+geonwoomun@users.noreply.github.com>
2020-10-28 17:35:32 +0900

    feat #12: Issue 목록 화면 구현 중
    issue 목록화면 구현 중입니다.
    issues와 issue 컴포넌트로 나누었습니다.
    useEffect를 통해 페이지가 렌더링 됐을 때 필요한 정보를 가져올 수 있도록 하였습니다.

--- frontend/src/pages/Main.jsx
+++ frontend/src/pages/Main.jsx
@@ -1,27 +1,43 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { axios } from '../api';
+import Issues from '../components/issues/Issues';
 
 const Main = () => {
+  const [myInfo, setMyInfo] = useState(null);
+  const [issues, setIssues] = useState([]);
+
   const checkAuthGithub = async () => {
     try {
-      const response = await axios.get('/auth/login/success', {
-        withCredentials: true,
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-          'Access-Control-Allow-Credentials': true
-        }
-      });
-      console.log(response);
+      const response = await axios.get('/auth/login/success');
+      setMyInfo(response.data.user);
     } catch (error) {
       console.log(error);
     }
   };
+
+  const getIssues = async () => {
+    try {
+      const response = await axios.get('/issues?issueTrackerId=1');
+      setIssues(response.data.issues);
+    }
+    catch(error) {
+      console.log(error);
+    }
+  };
+
   useEffect(() => {
     checkAuthGithub();
+    getIssues();
   }, []);
 
-  return <div>메인입니다.</div>;
+  return (
+    <div>
+      {myInfo && `안녕하세요. ${myInfo.username}님`}
+      {issues.length ? <Issues issues={issues}/> : <div>이슈가 없습니다.</div>
+      }
+    </div>
+
+  );
 };
 
 export default Main;
