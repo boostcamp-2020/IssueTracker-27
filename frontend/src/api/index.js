823fdbd7899518b2cfd6919cdb49c03373a514ed
geonwoomun <38036598+geonwoomun@users.noreply.github.com>
2020-10-28 17:33:34 +0900

    fix: custom Axios option 변경
    custom axios의 option을 변경해주었습니다.

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -3,5 +3,11 @@ import Axios from 'axios';
 export const BASE_URL = 'http://localhost:5000';
 
 export const axios = Axios.create({
-  baseURL: BASE_URL
+  baseURL: BASE_URL,
+  withCredentials: true,
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Credentials': true
+  }
 });
