625c319d7d0f2c45d2d56f5204646e6284400202
김종은 <jongeunkim@gimjong-eun-ui-MacBookPro.local>
2020-10-29 18:27:04 +0900

    docs #12: Issue에 대한 정보를 주석에 넣음
    Issue에 대한 정보들이 무엇이 있고 뭘 의미하는지 확인하기 편하게 주석에 적어놨다.

--- frontend/src/components/IssueTable/IssueList/Issue/index.jsx
+++ frontend/src/components/IssueTable/IssueList/Issue/index.jsx
@@ -2,11 +2,34 @@ import React from 'react';
 import {IssueItem} from './style';
 
 const Issue = ({issue}) => {
-    return (
-        <IssueItem>
-            {issue.title}{issue.description}
-        </IssueItem>
-    );
+  console.log(issue);
+  /**
+   * id
+   * description,
+   * openAt,
+   * status
+   * 
+   * issue.Assignees[i].id : 담당자 id
+   * issue.Assignees[i].JoinUser.id : issueTracker에 등록한 사용자의 id
+   * issue.Assignees[i].JoinUser.User.id : 사용자 id
+   * issue.Assignees[i].JoinUser.User.username : 사용자 닉네임
+   * issue.Assignees[i].JoinUser.User.profileImage : 사용자 프로필 사진
+   * 
+   * issue.Milestone.title : 마일스톤 제목  
+   * issue.Milestone.id : 마일스톤 id
+   * 
+   * issue.Labels[i].id : label id
+   * issue.Labels[i].title: label 제목
+   * 
+   * issue.status: issue open/close
+   * issue.openAt: issue 오픈날
+   */
+
+  return (
+    <IssueItem>
+      {issue.title} | {issue.id} | {issue.JoinUser.User.username} | 
+    </IssueItem>
+  );
 };
 
 export default Issue;
