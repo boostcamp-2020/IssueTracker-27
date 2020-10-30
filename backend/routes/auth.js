c07d13c88eebb34ae5c72ecaab06212c685f2164
김종은 <jongeunkim@gimjong-eun-ui-MacBookPro.local>
2020-10-30 12:19:05 +0900

    fix #21: 깃허브 callback의 url 주소 다시 수정
    상대결로가 아닌 url를 가져오는 파일명이 잘못되어서 수정하였다.

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,7 +2,7 @@ const express = require('express');
 const passport = require('passport');
 const router = express.Router();
 require('dotenv').config();
-const BASE_URL = require('../config/config')[process.env.ENV];
+const BASE_URL = require('../config/urlconfig')[process.env.ENV];
 
 router.get('/login/success', (req, res) => {
   if (req.user) {
@@ -29,7 +29,7 @@ router.get('/login/github', passport.authenticate('github'));
 router.get(
   '/login/github/callback',
   passport.authenticate('github', {
-    successRedirect: '/main',
+    successRedirect: `${BASE_URL.client}/main`,
     failureRedirect: '/',
   })
 );
