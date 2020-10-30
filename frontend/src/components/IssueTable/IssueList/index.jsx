991c5c58e8c56947c2c5c47c59173fd38a49722d
김종은 <jongeunkim@gimjong-eun-ui-MacBookPro.local>
2020-10-30 10:37:07 +0900

    fix: 공통 gitignore 파일 생성
    백엔드랑 프론트에서 따로 관리하던 gitignore를 상위 폴더에서 하나로 관리하도록 수정하였다.

--- frontend/src/components/IssueTable/IssueList/index.jsx
+++ frontend/src/components/IssueTable/IssueList/index.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import Issue from './Issue';
-import {IssueItems} from './style';
+import { IssueItems } from './style';
 
-const IssueList = ({issues}) => {
-    return (
-      <IssueItems>
-        {issues.map(issue => (
-          <Issue key={issue.id} issue={issue} />
-        ))}
-      </IssueItems>
-    );
+const IssueList = ({ issues }) => {
+  return (
+    <IssueItems>
+      {issues.map(issue => (
+        <Issue key={issue.id} issue={issue} />
+      ))}
+    </IssueItems>
+  );
 };
 
 export default IssueList;
