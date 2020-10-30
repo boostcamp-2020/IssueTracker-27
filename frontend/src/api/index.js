d9c7d57ff239e7b5cef8baf2f2501534312cb72d
김종은 <jongeunkim@gimjong-eun-ui-MacBookPro.local>
2020-10-30 12:02:47 +0900

    fix #21: 개발 환결에 따라 url이 변경되도록 프론트에 설정
    development일 경우 localhost를 사용하고 production일 경우 서버 IP 주소를 사용하도록 설정하였다.

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,6 +1,9 @@
 import Axios from 'axios';
 
-export const BASE_URL = 'http://localhost:5000';
+export const BASE_URL =
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:5000'
+    : 'http://118.67.130.104:5000';
 
 export const axios = Axios.create({
   baseURL: BASE_URL,
