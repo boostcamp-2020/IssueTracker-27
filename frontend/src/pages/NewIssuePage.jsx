183bbf751a1872e5cb3660bc40b21d035d0e48a0
김종은 <jongeunkim@gimjong-eun-ui-MacBookPro.local>
2020-11-03 10:20:19 +0900

    feat #23: 사용자 프로필 부분의 틀 구현
    이슈를 생성할 사용자의 프로필이 있을 부분을 대략 간단하게 틀만 잡아놓았다.

--- frontend/src/pages/NewIssuePage.jsx
+++ frontend/src/pages/NewIssuePage.jsx
@@ -1,16 +1,18 @@
 import React, { useEffect, useState } from 'react';
+import NewIssue from '../components/NewIssue';
 
 // SelectedAssignes,Selectedlabels, SelectedMilestone
 
 const NewIssuePage = () => {
   return (
     <>
-      <div>사진</div>
+      <NewIssue />
+      {/* <div>사진</div>
       <section>
         state 관리
         <form>화면 작성부분</form>
         <aside>Assignee Labels Milestone</aside>
-      </section>
+      </section> */}
     </>
   );
 };
