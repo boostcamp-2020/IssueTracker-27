ee76a9b7b12361c01eae5043ebe59e899529616c
김종은 <jongeunkim@gimjong-eun-ui-MacBookPro.local>
2020-11-03 10:22:11 +0900

    feat #23: 이슈 생성 컴포넌트 구현
    이슈 생성 컴퓨넌트를 잘게 나누어서 구현하였다.
    크게는 프로필 부분과 input section으로 나누고 input section은 다시
    form 부분과 추가적인 사항들을 넣을 수 있는 (label, milestone, assignee) 부분으로 나누어서 구현하였다.
    :

---
+++ frontend/src/components/NewIssue/InputSection/IssueForm.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import useInputChange from '../../../hooks/useInputChange';
+import { axios } from '../../../api';
+import { GreenButton } from '../../common/Buttons';
+import { useMainState } from '../../../contexts/MainContext';
+import { useHistory } from 'react-router-dom';
+
+const IssueForm = ({ additionalInfo }) => {
+  const [title, changeTitle] = useInputChange('');
+  const [description, changeDescription] = useInputChange('');
+  const { myInfo } = useMainState();
+  const history = useHistory();
+
+  const onSubmit = async e => {
+    e.preventDefault();
+    try {
+      const labelIds = additionalInfo.labels.map(label => label.id);
+      const assigneeIds = additionalInfo.assignees.map(assignee => assignee.id);
+      const milestone = additionalInfo.milestone?.id;
+
+      const body = {
+        joinUserId: myInfo.joinUserId,
+        issueTrackerId: 1,
+        title,
+        description,
+        milestone,
+        labelIds,
+        assigneeIds
+      };
+      const response = await axios.post('/api/issue', body);
+      history.push('/main');
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <input value={title} onChange={changeTitle}></input>
+      <textarea value={description} onChange={changeDescription}></textarea>
+      <button>Cancel</button>
+      <GreenButton width='200px'>Sumbit new issue</GreenButton>
+    </form>
+  );
+};
+
+export default IssueForm;
---
+++ frontend/src/components/NewIssue/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import InputSection from './InputSection';
+import ProfilePicture from './ProfilePicture';
+
+const NewIssue = () => {
+  return (
+    <>
+      <ProfilePicture />
+      <InputSection />
+    </>
+  );
+};
+
+export default NewIssue;
