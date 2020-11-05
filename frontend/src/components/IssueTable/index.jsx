6e2e6c45d8bbc8880853436332f93f914bcb1a44
김종은 <jongeunkim@gimjong-eun-ui-MacBookPro.local>
2020-11-05 18:49:58 +0900

    feat #12: useCheckState 구현
    모든 이슈들을 선택하고 선택해제하는 훅스를 구현하였다.
    이슈들을 각각 따로 선택할 경우 상단에 총 몇개의 이슈들이 선택됐는지 보여준다.

--- frontend/src/components/IssueTable/index.jsx
+++ frontend/src/components/IssueTable/index.jsx
@@ -1,14 +1,41 @@
-import React from 'react';
+import React, { useEffect, useMemo } from 'react';
 import styled from '@emotion/styled';
 import Toolbar from './Toolbar';
 import IssueList from './IssueList';
 import IssueTableContainer from './style';
+import useFilteredIssues from '@hooks/useFilteredIssues';
+import useCheckState from '@hooks/useCheckState';
 
 const IssueTable = () => {
+  const filteredIssues = useFilteredIssues();
+  const [
+    isCheckAll,
+    toggleIsCheckAll,
+    checkState,
+    setCheckState
+  ] = useCheckState(filteredIssues);
+
+  const checkedNumber = useMemo(
+    () =>
+      Object.keys(checkState).reduce(
+        (total, id) => total + (checkState[id] ? 1 : 0),
+        0
+      ),
+    [checkState]
+  );
+
   return (
     <IssueTableContainer>
-      <Toolbar />
-      <IssueList />
+      <Toolbar
+        isCheckAll={isCheckAll}
+        toggleIsCheckAll={toggleIsCheckAll}
+        checkedNumber={checkedNumber}
+      />
+      <IssueList
+        checkState={checkState}
+        setCheckState={setCheckState}
+        filteredIssues={filteredIssues}
+      />
     </IssueTableContainer>
   );
 };
