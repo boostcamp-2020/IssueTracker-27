b1a59b8b14dd5199dde758616d44bf27d0d9d58c
김종은 <jongeunkim@gimjong-eun-ui-MacBookPro.local>
2020-11-05 10:49:31 +0900

    fix: CORS 에러 해결
    CORS의 origin을 설정하였는데 127.0.0.1이랑 localhost를 서로 다른 주소로 인색해서 에러가 발생했다.
    해결하기 위해서 origin의 값을 배열 형식으로 두 주소를 모두 넣어줬다.

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ const app = express();
 process.env.ENV === 'development' &&
   app.use(
     cors({
-      origin: 'http://localhost:3000',
+      origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
       credentials: true,
     })
   );
