a657d4af978170cc9a797482b0292c718215d0fa
김종은 <jongeunkim@gimjong-eun-ui-MacBookPro.local>
2020-11-09 15:09:40 +0900

    feat #23: drop down 외의 공간에 클릭하면 닫히도록 구현
    drop down 메뉴를 열고 나서 그 외의 공간에 클릭하면 메뉴가 닫히도록 구현하였다.

--- frontend/src/components/common/SideBox.jsx
+++ frontend/src/components/common/SideBox.jsx
@@ -1,14 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import useToggle from '@hooks/useToggle';
 import styled from '@emotion/styled';
 
 const SideBoxWrapper = styled.div`
   position: relative;
-  height: 6rem;
-  width: 20rem;
-  border-bottom: 1px solid lightgrey;
+  width: 100%;
   padding: 1rem 0rem;
-  margin-left: 2rem;
+  border-bottom: 1px solid lightgrey;
 `;
 
 const SideBox = ({ children }) => {
@@ -16,14 +14,32 @@ const SideBox = ({ children }) => {
 };
 
 const DropDownWrapper = styled.div`
+  width: 100%;
   position: absolute;
   top: 3rem;
   left: 0;
   border: 1px solid lightgrey;
+  border-radius: 5px;
+  z-index: 1;
 `;
 
-const DropDownBox = ({ children }) => {
-  return <DropDownWrapper>{children}</DropDownWrapper>;
+const DropDownBox = ({ showToggle, children }) => {
+  const dropDownRef = useRef();
+
+  const closeDropDown = e => {
+    if (
+      dropDownRef.current &&
+      e.target.closest('div') !== dropDownRef.current
+    ) {
+      showToggle(prev => !prev);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('click', closeDropDown);
+  }, [dropDownRef]);
+
+  return <DropDownWrapper ref={dropDownRef}>{children}</DropDownWrapper>;
 };
 
 const SideBoxContainer = ({ FirstChild, SecondChild }) => {
@@ -32,7 +48,9 @@ const SideBoxContainer = ({ FirstChild, SecondChild }) => {
   return (
     <SideBox>
       {FirstChild(showToggle)}
-      {isShow && <DropDownBox>{SecondChild}</DropDownBox>}
+      {isShow && (
+        <DropDownBox showToggle={showToggle}>{SecondChild}</DropDownBox>
+      )}
     </SideBox>
   );
 };
