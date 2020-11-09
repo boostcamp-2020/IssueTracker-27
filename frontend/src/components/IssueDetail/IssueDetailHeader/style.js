cb282329e019cad9f2ffcd6628ad28354bbdc616
Jeongeun-Choi <cje3080@gmail.com>
2020-11-09 18:02:40 +0900

    style #45: IssueDetailHeader 스타일 생성

---
+++ frontend/src/components/IssueDetail/IssueDetailHeader/style.js
@@ -0,0 +1,28 @@
+import styled from '@emotion/styled';
+
+export const IssueDetailHeaderContainer = styled.header`
+  width: 100%;
+  border-bottom: 1px solid #d2d2d2;
+  display: flex;
+  justify-content: center;
+  & .issue_detail_header {
+    width: 95%;
+
+    & .issue_detail_header_top {
+      width: 100%;
+      display: flex;
+      justify-content: space-between;
+
+      & h1 {
+        & span {
+          color: #a6abb1;
+        }
+      }
+    }
+
+    & .issue_detail_header_bottom {
+      width: 100%;
+      display: flex;
+    }
+  }
+`;
