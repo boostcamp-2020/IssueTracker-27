f55e51f9da867bc924d7dfb0d4423091c7bc13f3
Jeongeun-Choi <cje3080@gmail.com>
2020-11-11 18:22:43 +0900

    feat #45: IssueDetail 생성

--- frontend/src/components/IssueDetail/index.jsx
+++ frontend/src/components/IssueDetail/index.jsx
@@ -1,28 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector } from '@hooks/react-context';
 import AsideMenu from '@components/AsideMenu';
 import IssueDetailHeader from './IssueDetailHeader';
-import IssueDetailMain from '@components/IssueDetail/IssueDetailMain';
+import IssueDetailMain from './IssueDetailMain';
+import Comment from './Comment';
 import { IssueDetailContainer } from './style';
 
 const IssueDetail = () => {
   const selectedIssue = useSelector(state => state.selectedIssue);
+  const { Assignees, IssueLabels, Milestone } = selectedIssue;
 
-  // const [additionalInfo, setAdditionalInfo] = useState({
-  //   assignees: selectedIssue
-  //   labels,
-  //   milestone: null
-  // });
+  const [additionalInfo, setAdditionalInfo] = useState({
+    assignees: Assignees,
+    labels: IssueLabels,
+    milestone: Milestone
+  });
+
+  useEffect(() => {
+    setAdditionalInfo({
+      assignees: Assignees,
+      labels: IssueLabels?.length && IssueLabels.map(label => label.Label),
+      milestone: Milestone
+    });
+  }, [selectedIssue]);
 
   return (
     <IssueDetailContainer>
       <IssueDetailHeader />
-      {/* <main>
+      <main>
         <div className='main_content'>
           <IssueDetailMain />
         </div>
-        <AsideMenu />
-      </main> */}
+        <AsideMenu
+          additionalInfo={additionalInfo}
+          setAdditionalInfo={setAdditionalInfo}
+        />
+      </main>
+      <Comment />
     </IssueDetailContainer>
   );
 };
