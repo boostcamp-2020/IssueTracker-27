9ed24cee926dd35beb3d16d44f421fe4245cf03b
Jeongeun-Choi <cje3080@gmail.com>
2020-11-11 18:26:15 +0900

    feat #45: IssueDetailContent 생성

--- frontend/src/components/IssueDetail/IssueDetailMain/IssueDetailContent/index.jsx
+++ frontend/src/components/IssueDetail/IssueDetailMain/IssueDetailContent/index.jsx
@@ -1,23 +1,39 @@
 import React from 'react';
+import { IssueDetailContentContainer } from './style';
 import { useSelector } from '@hooks/react-context';
 
 const IssueDetailContent = () => {
   const selectedIssue = useSelector(state => state.selectedIssue);
-  const { id, openAt, status, title, description, JoinUser } = selectedIssue;
+  const { id, openAt, title, description, JoinUser } = selectedIssue;
 
   return (
-    <>
-      <img
-        src={JoinUser?.User.profileImage}
-        alt='유저 이미지'
-        width={100}
-        height={100}
-      />
-      <div>
-        {title}
-        {description}
+    <IssueDetailContentContainer>
+      <div className='user_profile'>
+        <img
+          src={JoinUser?.User.profileImage}
+          alt='유저 이미지'
+          width={100}
+          height={100}
+        />
       </div>
-    </>
+      <section className='detail_content'>
+        <div className='detail_content_header'>
+          <div className='detail_content_header_left'>
+            <div>{title}</div>
+            <div>{openAt}</div>
+          </div>
+          <div className='detail_content_header_right'>
+            {JoinUser?.User.id && (
+              <>
+                <div>Owner</div>
+                <div>Edit</div>
+              </>
+            )}
+          </div>
+        </div>
+        <div className='detail_content_description'>{description}</div>
+      </section>
+    </IssueDetailContentContainer>
   );
 };
 
