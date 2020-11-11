2133a6f91e0f6f25d4aff488d45be4c375fe39d8
김종은 <jongeunkim@gimjong-eun-ui-MacBookPro.local>
2020-11-11 18:03:17 +0900

    feat #44: 하나의 레이블의 정보를 보여줄 LabelTile 컴포넌트 생성
    레이블에 대한 모든 정보들을 보여준다.
    버튼을 누를 경우 해당 레이블을 삭제한다.
    버튼을 누를 경우 레이블을 수정할 수 있는 폼을 보여주고
    변경된 사항을 저장할 수 있다.

--- frontend/src/components/LabelTable/LabelTile/index.jsx
+++ frontend/src/components/LabelTable/LabelTile/index.jsx
@@ -1,14 +1,45 @@
 import React from 'react';
+import { axios } from '@api';
+import { WhiteButton } from '@common/Buttons';
 import LabelTileContainer from './style';
+import { removeLabelAction } from '../../../contexts';
+import LabelForm from '../LabelForm';
+import useToggle from '@hooks/useToggle';
 
-const LabelTile = ({ label }) => {
+const LabelTile = ({ label, labelDispatch }) => {
+  const [isLabelShowing, toggleIsLabelShowing] = useToggle(false);
+
+  const deleteLabel = async () => {
+    try {
+      const { status } = await axios.delete(`api/label/${label.id}`);
+      if (status === 200) labelDispatch(removeLabelAction({ id: label.id }));
+      else throw Error('Not deleted');
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <LabelTileContainer bgcolor={label.color}>
-      <div className='label_tile_title'>{label.title}</div>
-      <div className='label_tile_description'>{label.description}</div>
-      <div>Edit</div>
-      <div>Delete</div>
-    </LabelTileContainer>
+    <>
+      <LabelTileContainer bgcolor={label.color}>
+        <div className='label_tile_title'>{label.title}</div>
+        <div className='label_tile_description'>{label.description}</div>
+        <WhiteButton width='5rem' onClick={toggleIsLabelShowing}>
+          Edit
+        </WhiteButton>
+        <WhiteButton width='5rem' onClick={deleteLabel}>
+          Delete
+        </WhiteButton>
+      </LabelTileContainer>
+      {isLabelShowing && (
+        <LabelForm
+          submitButtonName='Save changes'
+          type='updateLabel'
+          label={label}
+          toggleIsLabelShowing={toggleIsLabelShowing}
+        />
+      )}
+    </>
   );
 };
 
