af23623fe7e54822ecafeafa5e8b947206f9c4ee
김종은 <jongeunkim@gimjong-eun-ui-MacBookPro.local>
2020-11-12 22:07:03 +0900

    fix #59: NODE_ENV가 test일 경우 base url 변경
    NODE_ENV가 test일 경우 base url을 서버가 아닌 localhost가 되도록 수정하였다.

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,8 +1,8 @@
 import Axios from 'axios';
 
 export const BASE_URL =
-  process.env.NODE_ENV === 'development'
-    ? 'http://localhost:5000'
+  process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test'
+    ? 'http://127.0.0.1:5000'
     : 'http://118.67.130.104:5000';
 
 export const axios = Axios.create({
