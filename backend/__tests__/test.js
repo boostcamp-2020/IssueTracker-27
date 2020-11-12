fc57bf3f045c8faacd0d6b21bd9414748a237d64
김종은 <jongeunkim@gimjong-eun-ui-MacBookPro.local>
2020-11-12 15:33:08 +0900

    feat #58: POST 이슈 생성 에러 테스트
    이슈를 생성할 때 필수값인 title를 입력하지 않으면
    적절한 response code를 반환하는지 확인하였다.

--- backend/__tests__/test.js
+++ backend/__tests__/test.js
@@ -1,16 +1,43 @@
 const request = require('supertest');
 const app = require('../app');
 
-describe('GET /api/issue/:issueId - 특정 이슈 조회', () => {
-  it('GET: 1번 이슈를 들고온다.', async (done) => {
-    const res = await request(app).get('/api/issue/1').set({
-      'Content-Type': 'application/json',
-    });
+describe('ISSUE API TEST', () => {
+  let server;
 
-    const { body } = res;
-    expect(res.statusCode).toBe(200);
-    expect(body).toHaveProperty('issue');
-    expect(body.issue.id).toBe(1);
-    done();
+  beforeEach(() => {
+    server = request(app);
+  });
+
+  describe('GET /api/issue/1 - 특정 이슈 조회', () => {
+    it('1번 이슈를 볼러온다.', async (done) => {
+      const res = await server.get('/api/issue/1').set({
+        'Content-Type': 'application/json',
+      });
+      const { body } = res;
+      expect(res.statusCode).toBe(200);
+      expect(body).toHaveProperty('issue');
+      expect(body.issue.id).toBe(1);
+      done();
+    });
+  });
+
+  describe('POST /api/issue - 이슈 생성 ', () => {
+    it('title이 없을때 이슈가 생성되면 안된다.', async (done) => {
+      const res = await server
+        .post('/api/issue/')
+        .send({
+          joinUserId: 1,
+          description: 'test',
+          milestoneId: 1,
+          issueTrackerId: 1,
+        })
+        .set({
+          'Content-Type': 'application/json',
+        });
+      const { body } = res;
+      expect(res.statusCode).toBe(400);
+      expect(body.message).toBe('error');
+      done();
+    });
   });
 });
