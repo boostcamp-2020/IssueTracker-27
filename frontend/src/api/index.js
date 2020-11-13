1559f26249d363c5e8df53fc11183160f3e03c3c
김종은 <jongeunkim@gimjong-eun-ui-MacBookPro.local>
2020-11-13 10:25:24 +0900

    fix: 로그인 페이지에서는 user를 확인하지 않도록 수정
    로그인 페이지에서는 사용자가 로그인하기 전이라 확인할 필요가 없어서
    해당 페이지에서는 사용자가 로그인했는지 확인하는 로직을 제외하도록 했다.

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -2,7 +2,7 @@ import Axios from 'axios';
 
 export const BASE_URL =
   process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test'
-    ? 'http://127.0.0.1:5000'
+    ? 'http://localhost:5000'
     : 'http://118.67.130.104:5000';
 
 export const axios = Axios.create({
